1bc80e145284a4e795d9145f788229c7778e2e1d
lizhsen <lizhsen@163.com>
2018-01-10 22:22:57 +0800

    zs
    Signed-off-by: lizhsen <lizhsen@163.com>

--- learning_logss/views.py
+++ learning_logss/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
@@ -9,3 +10,19 @@ from django.shortcuts import render
 def index(request):
     """学习笔记的主页"""
     return render(request, 'learning_logss/index.html')
+
+
+def topics(request):
+    """显示所有的主题"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logss/topics.html', context)
+
+
+def topic(request,topic_id):
+    """显示单个主题以及所有的条目"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logss/topic.html', context)
+
