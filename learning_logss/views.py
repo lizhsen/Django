0aed89ff6fca2db209cb78b60cb5b7b7ebea326b
lizhsen <lizhsen@163.com>
2018-01-13 21:10:00 +0800

    new added projects

--- learning_logss/views.py
+++ learning_logss/views.py
@@ -2,7 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from .models import Topic
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -19,10 +22,64 @@ def topics(request):
     return render(request, 'learning_logss/topics.html', context)
 
 
-def topic(request,topic_id):
+def topic(request, topic_id):
     """显示单个主题以及所有的条目"""
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logss/topic.html', context)
+
+
+def new_topic(request):
+    """添加新主题"""
+    if request.method != 'POST':
+        # 未提交数据，创建一个新表单
+        form = TopicForm()
+    else:
+        # POST提交的数据，对数据进行处理
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logss:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logss/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """在特定的主题中添加新条目"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logss:topic',
+                                                args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logss/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logss:topic', args=[topic.id]))
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logss/edit_entry.html', context)
+
+
+
+
 
